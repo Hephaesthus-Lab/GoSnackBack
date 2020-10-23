dad94a763b8b84f498b5f0fafb160977afc87942
Hiran Junior <maximosdrr@gmail.com>
2020-10-23 15:55:28 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,17 @@ import { OrderModule } from './modules/order/order.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: ['ormConfig.env'], isGlobal: true }),
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [process.env.TYPEORM_ENTITIES],
+      synchronize: process.env.TYPEORM_SYNCHRONIZE == 'true' ? true : false,
+    }),
     ClientModule,
     ProductModule,
     OrderModule,
